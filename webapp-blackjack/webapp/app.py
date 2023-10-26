42042f48653005ca26fa8cbe13405425a9f0f30c
Daniel Carroll <Dancarrollsp@gmail.com>
2023-10-26 19:13:22 +0100

    Game now correctly displays the winner

--- webapp-blackjack/webapp/app.py
+++ webapp-blackjack/webapp/app.py
@@ -11,6 +11,18 @@ app = Flask(__name__)
 deck_values = init_deck_values()
 deck = make_deck()
 
+
+##Global Variables##
+canHit = True
+playerOver21 = False
+stood = False
+
+playerWon = False
+dealerWon = False
+drawGame = False
+gameOver = False
+
+
 def draw():
     """Select a random card from the deck. The deck shrinks by 1."""
     selection = random.choice(session["deck"])
@@ -18,20 +30,54 @@ def draw():
     session.modified = True
     return selection, deck_values[selection]
 
+def decide_winner():
 
-##Global Variables##
-canHit = True
-playerOver21 = False
-stood = False
+    ##Global variables
+    global playerWon
+    global dealerWon
+    global drawGame
+    
+    ##Gets card value totals
+    player_value = calc_total(session["player"])
+    dealer_value = calc_total(session["dealer"])
+
+    ##decide who wins here with if statements
+    if(player_value > dealer_value): 
+        playerWon = True
+        dealerWon = False
+        drawGame = False
+    if(player_value < dealer_value):
+        playerWon = False
+        dealerWon = True
+        drawGame = False
+
+    if(player_value > 21 and dealer_value <= 21):
+        playerWon = False
+        dealerWon = True
+        drawGame = False
+    if(player_value <= 21 and dealer_value > 21):
+        playerWon = True
+        dealerWon = False
+        drawGame = False
+
+    if(player_value > 21 and dealer_value > 21):
+        playerWon = False
+        dealerWon = False
+        drawGame = True
+    if(player_value == dealer_value):
+        playerWon = False
+        dealerWon = False
+        drawGame = True
 
 
 
-
-
-
+##########################################################################################################################################
 @app.get("/")
 @app.get("/start")
 def display_opening_state():
+
+    global stood 
+    stood = False
 
     session["deck"] = make_deck()
     session["player"] = []
@@ -53,7 +99,7 @@ def display_opening_state():
         footer="",
     )
 
-
+##########################################################################################################################################
 @app.post("/stand")
 def over_to_the_dealer():
 
@@ -61,61 +107,68 @@ def over_to_the_dealer():
     global canHit
     global stood
 
-    ####Player can no longer hit
-    ####Reveal dealers cards
-    ##The dealer will hit unitl they have a sum of 17 or higher
-    ##If the dealer goes over 21 dealer BUSTS
-    ##If the dealer has less than player the dealer BUSTS
+    global playerWon
+    global dealerWon
+    global drawGame
+    global gameOver
+
+
+    ##Player cant hit after standing
     canHit = False
     stood = True
+    gameOver = True
 
+    ##Dealer hits until value is 17 or more
     while (calc_total(session["dealer"]) <= 17):
         session["dealer"].append(draw())
         
 
+    ##Decide if player won, lost or drawed
+    decide_winner()
+    
 
-    return render_template(
-        "start.html",
-        player_cards=session["player"],
-        player_total=calc_total(session["player"]),
-        dealer_cards=session["dealer"],
-        dealer_total=calc_total(session["dealer"]),
-        dealer_total_text=calc_total(session["dealer"]),
-        title="",
-        header="",
-        footer="",
-    )
+    ##Return correct info
+    common_data = {
+        "player_cards": session["player"],
+        "player_total": calc_total(session["player"]),
+        "dealer_cards": session["dealer"],
+        "dealer_total": calc_total(session["dealer"]),
+        "dealer_total_text": calc_total(session["dealer"]),
+        "title": "",
+        "header": "",
+        "footer": "",
+    }
 
+    if playerWon:
+        common_data["player_won"] = playerWon
+
+    if dealerWon:
+        common_data["dealer_won"] = dealerWon
+
+    if drawGame:
+        common_data["draw_game"] = drawGame
+
+    return render_template("start.html", **common_data)
+
+
+
+
+##########################################################################################################################################
 @app.post("/hit")
 def select_another_card():
 
-    #variables
-    global canHit
-    global playerOver21
+    ##Global variables
+    global stood
     
-    ####If player hasnt stood
-    #### add a card to players hand
-    ##If player goes over 21 its the dealers turn
-    ##Dealer reveals card and hits until they reach 17 or more
-    ##If the dealer doesnt go over 21 player BUSTS
+    ##Gets card value totals
+    player_value = calc_total(session["player"])
 
-    if(calc_total(session["player"]) > 21):
-        playerOver21 = True
+    
+    ##Add a card to the player hand if they havent gone over 21
+    if(player_value <= 21 and stood == False): session["player"].append(draw())
 
 
-    if(playerOver21 == False):##If player hasnt stood
-        use_player_total = calc_total(session["player"])##Add card to player hand
-
-
-        if(use_player_total <= 21):
-            ##Player hasnt busted
-            session["player"].append(draw())
-        else:
-            ##Player busted
-            temp = 1
-
-
-
+    ##With dealer calc total
     if(stood == False):
         return render_template(
         "start.html",
@@ -126,9 +179,8 @@ def select_another_card():
         title="",
         header="",
         footer=""
-    )
-
-    if(stood == True):
+    ) 
+    else:##Without dealer calc total
         return render_template(
         "start.html",
         player_cards=session["player"],
@@ -140,7 +192,7 @@ def select_another_card():
         header="",
         footer="",
     )
-
+##########################################################################################################################################
 
 
 
